9c9c5c46760d31eddb117ef946e57074f71ee444
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-01-15 10:59:19 -0400

    Fix task 6 pycodestyle

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str1 = "Python "
-str2 = "is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str2 = ("is an interpreted, interactive, object-oriented programming "
+        "language that combines remarkable power with very clear syntax")
 str = str1 + str2
 print(str[39:66] + str[106:112] + str1[:6])
