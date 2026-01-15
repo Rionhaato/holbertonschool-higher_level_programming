92914a24272d567f6b04c28e7a7cbb7580472d8d
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-01-15 10:41:35 -0400

    Fix task 6 concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Python "
 str2 = "is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str1 = str2[32:59] + str2[59:60] + str2[100:105] + str1[0:6]
+str1 = str2[32:59] + str2[59:60] + str2[100:105] + str1[:6]
 print(str1)
