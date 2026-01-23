431f59004e530d64a934deb3a3685d9856d8f1db
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-01-23 14:28:53 -0400

    Add 0-safe_print_list.py which prints x elemets of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i], end="")
+            printed += 1
+        except IndexError:
+            break
+        i += 1
+    print()
+    return printed
