bae4aad91310f50806a47c357b16549afc0e9477
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-01-24 18:16:26 -0400

    Task 0: Add add_integer witg doctests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add two numbers as integers.
+
+This module provides a function for adding two numbers.
+Both arguments must be integers or floats.
+Floats are cast to integers before addition.
+"""
+
+
+def add_integer(a, b=98):
+    """Return the integer sum of two numbers.
+
+    Args are cast to integers before addition.
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
