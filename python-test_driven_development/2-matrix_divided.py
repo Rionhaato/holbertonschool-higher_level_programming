706a33a05a9b9d0ae26611fa6a66abf8758fa4c0
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-01-24 19:35:06 -0400

    task1: update matrix_divided doctests and pycodestyle fixes

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -33,7 +33,8 @@ def matrix_divided(matrix, div):
         for item in row:
             if type(item) not in (int, float):
                 raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats"
+                    "matrix must be a matrix (list of lists) "
+                    "of integers/floats"
                 )
 
     if type(div) not in (int, float):
