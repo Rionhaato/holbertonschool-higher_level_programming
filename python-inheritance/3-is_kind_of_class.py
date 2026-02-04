f187770641881eb1a7deb2cabbd2e0c513281d5f
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-02-04 11:04:14 -0400

    Add is_kind_of_class() for instance/subclass checks

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that checks class or subclass match."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or a subclass."""
+    return isinstance(obj, a_class)
