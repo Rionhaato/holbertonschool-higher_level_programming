7aa3f45d63cd4443c5255e890e162b5dba95160c
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-02-04 11:04:36 -0400

    Add BaseGeometry.area() placeholder exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines BaseGeometry with area."""
+
+
+class BaseGeometry:
+    """Base geometry class with area stub."""
+
+    def area(self):
+        """Raise an exception for unimplemented area."""
+        raise Exception("area() is not implemented")
