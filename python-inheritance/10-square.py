83ba569ecf15f35dc9966fc0b619ac721b4a17a2
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-02-04 11:05:56 -0400

    Add Square size validation and area

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines Square."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square that reuses Rectangle behavior."""
+
+    def __init__(self, size):
+        """Initialize a square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
