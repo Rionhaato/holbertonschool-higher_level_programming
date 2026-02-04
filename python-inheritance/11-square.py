357b6d6bd0e63697e4cd3011a2c7b02c7b2f6812
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-02-04 11:06:20 -0400

    Add Square __str__ description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines Square with string form."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square that overrides string form."""
+
+    def __init__(self, size):
+        """Initialize a square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the string form of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
