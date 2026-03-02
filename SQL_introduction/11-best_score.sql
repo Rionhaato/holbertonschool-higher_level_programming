ebfa1ef4d44a309588340593303519f31e0f053d
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-03-02 11:20:30 -0400

    Task 11: select best scores

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List records from second_table where score is at least 10
+-- Selects score and name for scores greater than or equal to 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
