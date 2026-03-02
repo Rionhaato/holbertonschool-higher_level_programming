8d09bf2cdbb2907ac5bd5d8ae880915fc241b1e0
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-03-02 10:43:17 -0400

    Task 10: list by top score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- List all records from second_table ordered by score descending
+-- Selects score and name sorted from highest score to lowest
+SELECT score, name FROM second_table ORDER BY score DESC;
