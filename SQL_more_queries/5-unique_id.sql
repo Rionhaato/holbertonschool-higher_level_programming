1e125c41434d381413cb319061655b5be5472b99
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-03-06 11:30:57 -0400

    Task 5: Unique ID

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table unique_id.
+-- id has a default value of 1 and must be unique.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
