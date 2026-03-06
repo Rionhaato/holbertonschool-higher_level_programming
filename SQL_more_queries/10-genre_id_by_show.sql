122535b4f6740669f022277d96ed9888e87c4a15
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-03-06 11:47:17 -0400

    Task 10: Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Script that lists all shows with at least one linked genre.
+-- Results are sorted by show title and genre id in ascending order.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
