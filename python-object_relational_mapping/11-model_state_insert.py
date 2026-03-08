8dff3a6fe390247e9bcf009ea5bb238d613a1f4b
Rionhaato <joshua.d.santiago92@gmail.com>
2026-03-08 09:49:41 -0400

    Task 11: add script to insert State Louisiana

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Adds the State object Louisiana to the database."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import sys
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
+    session.close()
