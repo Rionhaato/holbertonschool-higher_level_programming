7520a2f3e4738ccf90d6ea14601062788cbb2f15
Rionhaato <joshua.d.santiago92@gmail.com>
2026-03-08 15:22:27 -0400

    Chore: set executable mode for ORM scripts

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the City class and its relationship to State."""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
+
+
+class City(Base):
+    """Represents a city for a MySQL database."""
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
