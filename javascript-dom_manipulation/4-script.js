458da9e5983ab32494642973b04cf17044b86f40
Rionhaato <joshua.d.santiago92@gmail.com>
2026-03-24 16:44:50 -0400

    Task 4: add list item on click

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.querySelector('#add_item').addEventListener('click', function () {
+  const item = document.createElement('li');
+
+  item.textContent = 'Item';
+  document.querySelector('.my_list').appendChild(item);
+});
