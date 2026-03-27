215cc9bde747660c6a12098130d09233f81b3b22
Rionhaato <joshua.d.santiago92@gmail.com>
2026-03-27 17:10:34 -0400

    Task 2: add red class to header on click

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,6 @@
-document.querySelector('#red_header').addEventListener('click', function () {
-  document.querySelector('header').classList.add('red');
+const redHeaderControl = document.querySelector('#red_header');
+const headerElement = document.querySelector('header');
+
+redHeaderControl.addEventListener('click', () => {
+  headerElement.classList.add('red');
 });
