ec6b393fedd703be3022d5729abdc75aff615038
Rionhaato <joshua.d.santiago92@gmail.com>
2026-03-27 17:19:15 -0400

    Task 7: fetch and list Star Wars movies

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,14 +1,11 @@
 fetch('https://swapi-api.hbtn.io/api/films/?format=json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const list = document.querySelector('#list_movies');
+  .then(response => response.json())
+  .then((movieData) => {
+    const moviesList = document.querySelector('#list_movies');
 
-    data.results.forEach(function (movie) {
-      const item = document.createElement('li');
-
-      item.textContent = movie.title;
-      list.appendChild(item);
+    movieData.results.forEach((film) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = film.title;
+      moviesList.appendChild(listItem);
     });
   });
